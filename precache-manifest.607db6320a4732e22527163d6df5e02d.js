4b78b8daa86f3cf934865d3c91b5eee0f1d8c7e6
Kory Taborn <kory.taborn@gmail.com>
2019-11-27 10:13:46 -0600

    Updates

---
+++ precache-manifest.607db6320a4732e22527163d6df5e02d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6031865f86e8c200ebff0b86e0d391",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a77a3de96e63bda36478",
+    "url": "/monsters-rolodex/static/css/main.c03e4bcd.chunk.css"
+  },
+  {
+    "revision": "5e404e4e23ac080fa4fb",
+    "url": "/monsters-rolodex/static/js/2.e4bd5644.chunk.js"
+  },
+  {
+    "revision": "a77a3de96e63bda36478",
+    "url": "/monsters-rolodex/static/js/main.2356faa0.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
